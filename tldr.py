74debbf71bf88b6dc815509046d2c006aa334a38
zhujh098 <70138659+zhujh098@users.noreply.github.com>
2024-11-28 13:22:06 +0100

    Update tldr.py

--- tldr.py
+++ tldr.py
@@ -64,6 +64,7 @@ def get_paper_tldr(paper:arxiv.Result, model:Llama) -> str:
             }
         ],
         top_k=0,
-        temperature=0
+        temperature=0, 
+        max_tokens = 8192
     )
     return response['choices'][0]['message']['content']
